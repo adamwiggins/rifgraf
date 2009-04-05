c4e4bb16fa3583b42656ed37ad8a950fdf377e2f
Michael <fairchild@stimble.net>
2009-04-04 23:37:54 -0700

    add rackup file and gems manifest. Delete local gems.

--- sequel/lib/sequel.rb
+++
@@ -1,1 +0,0 @@
-require 'sequel_model'
--- sequel/spec/adapters/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-unless Object.const_defined?('Sequel')
-  $:.unshift(File.join(File.dirname(__FILE__), "../../lib/"))
-  require 'sequel_core'
-  Sequel.quote_identifiers = false
-end
-require File.join(File.dirname(__FILE__), '../spec_config.rb')
